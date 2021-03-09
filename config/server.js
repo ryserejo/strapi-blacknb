647df93d502f3ea5180d06c9badefccdef715d2b
Rynalde Serejo <47606325+ryserejo@users.noreply.github.com>
2021-03-09 08:59:45 -0300

    upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4612ba704148ca6fb550bfa00f6a2f17'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bc5bff16-53c3-4bb2-98b8-1cf8653c3c3b'
+};
